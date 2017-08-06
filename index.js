a0144834e5df8b1f8ec66df04f8813756469d055
wenqi0927 <1272379959@qq.com>
2017-08-06 15:52:58 +0800

    简单的例子

---
+++ index.js
@@ -0,0 +1,7 @@
+require('chromedriver')//创建浏览器
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder().forBrowser('chrome').build();//创建一个
+driver.get("http://www.baidu.com");//打开百度网页
+driver.findElement({id:"kw"}).sendKeys("F1");
+driver.findElement({id:"su"}).click();
+driver.quit();//关闭浏览器
